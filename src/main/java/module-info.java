ccb7a307bb45b1e623eba1fa06cb645e9e9cf945
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-11-26 13:17:18 +0300

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,16 +1,13 @@
 module com.tugalsan.api.servlet.http {
     requires jdk.httpserver;
-    
 //    requires com.tugalsan.api.crypto;
     requires com.tugalsan.api.function;
-    
     requires com.tugalsan.api.tuple;
     requires com.tugalsan.api.union;
     requires com.tugalsan.api.charset;
     requires com.tugalsan.api.url;
     requires com.tugalsan.api.network;
     requires com.tugalsan.api.log;
-    
     requires com.tugalsan.api.file;
     requires com.tugalsan.api.unsafe;
     requires com.tugalsan.api.stream;
